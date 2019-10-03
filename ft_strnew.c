7079d1f30f4a5e69205bf0f9481b79bbfb5185cb
Wiseguy Poudre <wpoudre@vo-i6.21-school.ru>
2019-10-03 19:46:00 +0300

    final_1

--- ft_strnew.c
+++ ft_strnew.c
@@ -16,6 +16,8 @@ char		*ft_strnew(size_t size)
 {
 	char	*new;
 
+	if((size + 1) < size)
+		return (NULL);
 	if (!(new = malloc(sizeof(char) * (size + 1))))
 		return (NULL);
 	ft_bzero(new, size + 1);
